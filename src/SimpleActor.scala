6250c5cd3a35e1fe0cd33017c45b267a335303ea
msameer <msameer@marinsoftware.com>
2017-02-20 13:44:10 -0800

    Simple Actor

---
+++ src/SimpleActor.scala
@@ -0,0 +1,18 @@
+import akka.actor.{Props, ActorSystem, Actor}
+
+object SimpleActor extends App {
+  class Reader extends Actor {
+    override def receive = {
+      case n: Number => println("Number:" + n)
+      case s: String => println("String:" + s)
+    }
+  }
+
+  val system = ActorSystem("SimpleActorSystem")
+  val myActor1 = system.actorOf(Props[Reader], "MyActor1")
+  val myActor2 = system.actorOf(Props[Reader], "MyActor2")
+  myActor1 ! 10
+  myActor1 ! "Sameer"
+  myActor2 ! 20
+  myActor2 ! "Samrah"
+}
